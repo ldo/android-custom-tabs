f4fcbbcbc4998a48c4c744ee13e27bccbb2db0a6
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2012-02-14 11:55:16 +0000

    copy of original source tree

---
+++ src/com/joshclemm/android/tabs/CustomTabActivity.java
@@ -0,0 +1,53 @@
+package com.joshclemm.android.tabs;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.TabHost.TabContentFactory;
+import android.widget.TabHost.TabSpec;
+
+public class CustomTabActivity extends Activity {
+
+	private TabHost mTabHost;
+
+	private void setupTabHost() {
+		mTabHost = (TabHost) findViewById(android.R.id.tabhost);
+		mTabHost.setup();
+	}
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// construct the tabhost
+		setContentView(R.layout.main);
+
+		setupTabHost();
+		mTabHost.getTabWidget().setDividerDrawable(R.drawable.tab_divider);
+
+		setupTab(new TextView(this), "Tab 1");
+		setupTab(new TextView(this), "Tab 2");
+		setupTab(new TextView(this), "Tab 3");
+	}
+
+	private void setupTab(final View view, final String tag) {
+		View tabview = createTabView(mTabHost.getContext(), tag);
+
+		TabSpec setContent = mTabHost.newTabSpec(tag).setIndicator(tabview).setContent(new TabContentFactory() {
+			public View createTabContent(String tag) {return view;}
+		});
+		mTabHost.addTab(setContent);
+
+	}
+
+	private static View createTabView(final Context context, final String text) {
+		View view = LayoutInflater.from(context).inflate(R.layout.tabs_bg, null);
+		TextView tv = (TextView) view.findViewById(R.id.tabsText);
+		tv.setText(text);
+		return view;
+	}
+}
